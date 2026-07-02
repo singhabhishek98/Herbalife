870c0b8662d740124aeaf6b00d99f55b4656cb13
abhishek <abhishek@plutos.one>
2026-07-02 23:06:49 +0530

    app.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,15 @@ async function connectDb() {
     throw new Error('MONGODB_URI is not configured');
   }
 
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection;
+  }
+
   await mongoose.connect(uri, {
     dbName: process.env.MONGODB_DB_NAME || 'herbalife'
   });
+
+  return mongoose.connection;
 }
 
 module.exports = connectDb;
